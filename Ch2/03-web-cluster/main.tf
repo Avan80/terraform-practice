c5a5d36a06811dcb80955acdbdf151a153e477ff
Avan80 <mdab7253@gmail.com>
2026-05-24 20:17:36 +0300

    Multiple instances with count and user scripts

---
+++ Ch2/03-web-cluster/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "web_cluster" {
+  count         = 3
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello from server $(hostname -f)" > index.html
+              python3 -m http.server 8080 &
+              EOF
+
+  tags = {
+    Name = "web-server-${count.index}"
+  }
+}
